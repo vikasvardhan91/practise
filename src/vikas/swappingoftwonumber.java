2a560cc9b3950b4be9c59fc07610e375147edab8
VIKAS <you@example.com>
2022-02-02 23:04:46 +0530

    swapping of two numbers

---
+++ src/vikas/swappingoftwonumber.java
@@ -0,0 +1,20 @@
+package vikas;
+
+public class swappingoftwonumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=10;
+		int y=20;
+		
+		
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		
+		System.out.println(x+"  "+y);
+		
+	}
+
+}
